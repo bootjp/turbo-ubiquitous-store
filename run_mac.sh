35cf5573dd7df43d395ff66862b69b124eec1c49
ぶーと / Yoshiaki Ueda <oh@bootjp.me>
2019-12-16 11:18:52 +0900

    fix run_mac.sh

--- run_mac.sh
+++ run_mac.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 env PRIMARY_REDIS_HOST=localhost:63790 env SECONDARY_REDIS_HOST=localhost:63791 env MASTER_REDIS_HOST=localhost:6379 go run distinct_executor/cli/main.go &
+PID=$!
 env PRIMARY_REDIS_HOST=localhost:63790 env SECONDARY_REDIS_HOST=localhost:63791 env MASTER_REDIS_HOST=localhost:6379 go run kvs/cli/server.go
-kill $!
+kill $PID
